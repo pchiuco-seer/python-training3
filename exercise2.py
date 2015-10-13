f37bb14986415dd58490bbf763ad8e6a182cdf80
Patrick Chiuco <patrick.chiuco@seer-technologies.com>
2015-10-13 14:50:25 +0800

    exercise done

--- exercise2.py
+++ exercise2.py
@@ -1,27 +1,25 @@
 vals = []
 input = ""
+output = {}
 while input != 'end':
 	input = raw_input("Enter a number:")
 	if (input.isdigit()):
 		vals.append(int(input))	
-	
 
-opt = raw_input("Enter an option:")
+output["m"] = map (lambda x: x*0.0254,vals)
+output["km"] = output["m"]/1000
+output["cm"] = output["m"]*100
+output["feet"] = map (lambda x: x*0.08333,vals)
+output["yard"] = map (lambda x: x*0.02778,vals)
 
-if opt == "km":
-	converted_vals = map (lambda x: x*0.0000254,vals)
-elif opt == "m":
-	converted_vals = map (lambda x: x*0.0254,vals)
-elif opt == "cm":
-	converted_vals = map (lambda x: x*2.54,vals)
-elif opt == "feet":
-	converted_vals = map (lambda x: x*0.08333,vals)
-elif opt == "yard":
-	converted_vals = map (lambda x: x*0.02778,vals)
+opt = str(raw_input("Enter an option:"))
+
+if(opt in output.keys()):
+	print output[str(opt)]
 else:
 	print "Invalid option"
 
-print converted_vals
+
 
 
 
