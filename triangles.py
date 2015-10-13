f8bc3d9111c7af1cb76eddff7242d7229723f24a
Patrick Chiuco <patrick.chiuco@seer-technologies.com>
2015-10-13 12:27:49 +0800

    triangles a and b done

--- triangles.py
+++ triangles.py
@@ -1,15 +1,49 @@
-def print_triangle(height):
+def print_half_triangle(height):
 	ones = 1
-	for h in range(0,height):
+	for h in xrange(0,height):
 		line = ""
-		for x in range(0,ones):
+		for x in xrange(0,ones):
 			line +=  '1'
-		for y in range(0,height-ones):
+		for y in xrange(0,height-ones):
 			line += ' '
 		ones += 1
 		line += ''
 		print line
-	#return
+
+def print_pyramid_a(height):
+	index = 1
+	for h in xrange(0,height):
+		line = ""
+		for x in xrange(0,(height - (h+1))):
+			line += ' '
+		for y in xrange(0,index):
+			line += '1'
+		for z in xrange(0,(height - (h+1))):
+			line += ' '
+		index += 2
+		print line
+
+def print_pyramid_b(height):
+	index = 1
+	for h in xrange(0,height):
+		line = ""
+		for x in xrange(0,(height - (h+1))):
+			line += ' '
+		for y in xrange(0,index):
+			if y % 2 == 0:
+			    line += '1'
+			else:
+				line += ' '
+		for z in xrange(0,(height - (h+1))):
+			line += ' '
+		
+		index += 2
+		print line
+
 
 input = int(raw_input("Enter height:"))
-print_triangle(input)
+print_half_triangle(input)
+print '\n'
+print_pyramid_a(input)
+print '\n'
+print_pyramid_b(input)
