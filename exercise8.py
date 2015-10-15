5d68f625ac7e8d2a632fdfb2656f0f0f8bf1cd95
Patrick Chiuco <patrick.chiuco@seer-technologies.com>
2015-10-15 09:50:06 +0800

    redefined algo for exercise8

--- exercise8.py
+++ exercise8.py
@@ -18,24 +18,35 @@ def same_char_instance(input):
 	instances_per_word = []
 
 	for x in xrange(0,len(input)):
-		unique = set(input[x])
+		str_buffer = input[x][0]
+		curr = input[x][0]
+		y = 1
 		instances = {}
-		for z in unique:
-			groups = input[x].split(z)
-			groups = [i for i in groups if i != '']
-			for y in groups:
-				if len(y) == 2:
-					if y in instances.keys():
-						instances[y] += 1
+		while y < len(input[x]):
+			print y
+			if input[x][y] == curr:
+				str_buffer += input[x][y]
+				if y == len(input[x])-1:
+					if len(str_buffer) == 2:
+						if str_buffer in instances.keys():
+							instances[str_buffer] += 1
+						else:
+							instances[str_buffer] = 1
+				y += 1
+			else:
+				curr = input[x][y]
+				if len(str_buffer) == 2:
+					if str_buffer in instances.keys():
+						instances[str_buffer] += 1
 					else:
-						instances[y] = 1
+						instances[str_buffer] = 1
+				str_buffer = curr
+				y += 1
+			print str_buffer
 		instances_per_word.append(instances)
 	return instances_per_word	
 
-
-
-list_input = ["apple","bub","dud","string","10010001001"]
-
+list_input = ["aabbccbbaa","apple","bub","dud","string","10010001001"]
 
 print unique_chars(list_input)
 print same_char_instance(list_input)
